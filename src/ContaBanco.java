89c95cafb1e29886dde543ece0d93b1970574212
Juliana Ribeiro <julianari.mendes@gmail.com>
2025-07-01 07:22:07 -0300

    Atualizar o ContaBanco.java

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -4,24 +4,19 @@ public class ContaBanco {
       public static void main(String[] args) {
         
             Scanner scanner = new Scanner(System.in);
-            // Solicitação para que o usuário digite o número da agência para armazenar a
-            // variável "agencia".
+           
             System.out.println("Por favor, digite o número da Agência: ");
             String agencia = scanner.nextLine();
-            // Solicitação para que o usuário digite o número da conta para armazenar a
-            // variável "numeroConta".
+           
             System.out.println("Por favor, digite o número da Conta: ");
             String numeroConta = scanner.nextLine();
-            // Solicitação para que o usuário digite o nome do cliente para armazenar a
-            // variável "nomeCliente".
+
             System.out.println("Por favor, digite o nome do Cliente: ");
             String nomeCliente = scanner.nextLine();
-            // Solicitação para que o usuário digite o saldo da conta para armazenar a
-            // variável "saldo".
+           
             System.out.println("Por favor, digite o saldo da conta: ");
             double saldo = scanner.nextDouble();
 
-            // Exibe a mensagem com os dados fornecidos pelo usuário.
             System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                         + agencia + ", conta " + numeroConta + " e seu saldo " + saldo
                         + " já está disponível para saque.");
