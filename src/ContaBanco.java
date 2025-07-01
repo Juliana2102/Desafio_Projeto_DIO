ec8fa7f15f1169a2e5c5b884723218a87371a417
Juliana Ribeiro <julianari.mendes@gmail.com>
2025-07-01 07:22:53 -0300

    Atualizar o ContaBanco.java

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -20,7 +20,7 @@ public class ContaBanco {
             System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                         + agencia + ", conta " + numeroConta + " e seu saldo " + saldo
                         + " já está disponível para saque.");
-            // Fechar o scanner
+            
             scanner.close();
       }
 }
